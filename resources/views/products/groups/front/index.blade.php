2c559c59e6b8e88bd7b369ccda8203fef1c05f25
Alexis Lopez <roalexis1234567890@gmail.com>
2022-03-09 23:31:00 -0600

    Crud categorias y vista principal terminados

--- resources/views/products/groups/front/index.blade.php
+++ resources/views/products/groups/front/index.blade.php
@@ -1,17 +1,18 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 py-8">
         {{-- grid de los post publicados --}}
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-8">
             @foreach ($groups as $group)
             <div class="flex flex-col items-center justify-center w-full">
-                <div class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md" style="background-image: url(https://images.unsplash.com/photo-1521903062400-b80f2cb8cb9d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80)"></div>
+                
+                <a href="/" class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-xl shadow-md" style="background-image: url(@if($group->image) {{Storage::url($group->image->url)}} @else https://cdn.pixabay.com/photo/2022/01/29/16/21/fisherman-6977946_960_720.jpg @endif)">
+                
+                </a>
         
-                <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
-                    <h3 class="py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">{{$group->name}}</h3>
+                <div class="w-56 -mt-10 overflow-hidden bg-orange-500 rounded-t-lg shadow-lg md:w-64 ">
+                    <h3 class="py-2 font-bold tracking-wide text-center text-white uppercase ">{{$group->name}}</h3>
                     
-                    <div class="flex items-center justify-between px-3 py-2 bg-gray-200 dark:bg-gray-700">
-                        <button class="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">Ver más</button>
-                    </div>
+                    
                 </div>
             </div> 
             @endforeach
