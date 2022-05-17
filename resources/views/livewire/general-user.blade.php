4ea1462b760100eedf1c05297dbcac917c38a262
Alexis Lopez <roalexis1234567890@gmail.com>
2022-05-17 00:03:40 -0500

    Ajuste en interfaz del usuario en el dashboard

--- resources/views/livewire/general-user.blade.php
+++ resources/views/livewire/general-user.blade.php
@@ -198,72 +198,70 @@
                 </div>
             </a>
         @endcan
+
+        {{-- card Buzon --}}
+        @can('ver-historico')
+            <a href="{{ route('buzon.index') }}" class="w-full">
+                <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white hover:bg-indigo-200">
+                    <div class="p-3 rounded-full bg-indigo-600   bg-opacity-75">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
+                        </svg>
+                    </div>
+
+                    <div class="mx-5">
+                        <h4 class="text-2xl font-semibold text-gray-700">Buzón</h4>
+                        <div class="text-gray-500">Realizar una Denuncia, Queja o Sugerencia</div>
+                    </div>
+                </div>
+            </a>
+
+
+            {{-- card Mercado Libre --}}
+            <a href="javascript:void(0)" class="w-full">
+                <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white hover:bg-indigo-200">
+                    <div class="p-3 rounded-full bg-indigo-600 bg-opacity-75">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                        </svg>
+                    </div>
+
+                    <div class="mx-5">
+                        <h4 class="text-2xl font-semibold text-gray-700">Mercado Libre</h4>
+                        <div class="text-gray-500">Visita nuestra tienda en línea y conoce más </div>
+                    </div>
+                </div>
+            </a>
+
+            {{-- card Amazon --}}
+            <a href="javascript:void(0)" class="w-full">
+                <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white hover:bg-indigo-200 ">
+                    <div class="p-3 rounded-full bg-indigo-600 bg-opacity-75">
+                        <svg class="h-8 w-8 text-white" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z" fill="currentColor"
+                                stroke="currentColor" stroke-width="2" stroke-linejoin="round"></path>
+                            <path
+                                d="M9.79999 8.4C9.79999 6.08041 11.6804 4.2 14 4.2C16.3196 4.2 18.2 6.08041 18.2 8.4V12.6C18.2 14.9197 16.3196 16.8 14 16.8C11.6804 16.8 9.79999 14.9197 9.79999 12.6V8.4Z"
+                                stroke="currentColor" stroke-width="2"></path>
+                        </svg>
+                    </div>
+
+                    <div class="mx-5">
+                        <h4 class="text-2xl font-semibold text-gray-700 ">Amazon</h4>
+                        <div class="text-gray-500">Productos con la mejor calidad</div>
+                    </div>
+                </div>
+            </a>
+        @endcan
     </div>
     {{-- End: Vista de usuarios administradores --}}
 
     {{-- Start: Vista de usuarios generales --}}
     @can('ver-historico')
-        <div class="mt-4">
-            <div class="flex flex-wrap -mx-6">
-                {{-- card Buzon --}}
-                <a href="{{ route('buzon.index') }}" class="w-full px-6 sm:w-1/2 xl:w-1/3">
-                    <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white hover:bg-indigo-200">
-                        <div class="p-3 rounded-full bg-indigo-600   bg-opacity-75">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z" />
-                            </svg>
-                        </div>
-
-                        <div class="mx-5">
-                            <h4 class="text-2xl font-semibold text-gray-700">Buzón</h4>
-                            <div class="text-gray-500">Realizar una Denuncia, Queja o Sugerencia</div>
-                        </div>
-                    </div>
-                </a>
-
-                {{-- card Mercado Libre --}}
-                <a href="javascript:void(0)" class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/3 sm:mt-0">
-                    <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white hover:bg-indigo-200">
-                        <div class="p-3 rounded-full bg-indigo-600 bg-opacity-75">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                            </svg>
-                        </div>
-
-                        <div class="mx-5">
-                            <h4 class="text-2xl font-semibold text-gray-700">Mercado Libre</h4>
-                            <div class="text-gray-500">Visita nuestra tienda en línea y conoce más </div>
-                        </div>
-                    </div>
-                </a>
-
-                {{-- card Amazon --}}
-                <a href="javascript:void(0)" class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/3 xl:mt-0 ">
-                    <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white hover:bg-indigo-200 ">
-                        <div class="p-3 rounded-full bg-indigo-600 bg-opacity-75">
-                            <svg class="h-8 w-8 text-white" viewBox="0 0 28 28" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z" fill="currentColor"
-                                    stroke="currentColor" stroke-width="2" stroke-linejoin="round"></path>
-                                <path
-                                    d="M9.79999 8.4C9.79999 6.08041 11.6804 4.2 14 4.2C16.3196 4.2 18.2 6.08041 18.2 8.4V12.6C18.2 14.9197 16.3196 16.8 14 16.8C11.6804 16.8 9.79999 14.9197 9.79999 12.6V8.4Z"
-                                    stroke="currentColor" stroke-width="2"></path>
-                            </svg>
-                        </div>
-
-                        <div class="mx-5">
-                            <h4 class="text-2xl font-semibold text-gray-700 ">Amazon</h4>
-                            <div class="text-gray-500">Productos con la mejor calidad</div>
-                        </div>
-                    </div>
-                </a>
-
-            </div>
-        </div>
 
         {{-- Espaciado entre cards y tabla --}}
         <div class="flex flex-col mt-8">
@@ -364,9 +362,7 @@
                                                     </a>
                                                 </td>
                                             </tr>
-                                            @include(
-                                                'buzon.front.modales.infoDenunciaUser'
-                                            )
+                                            @include('buzon.front.modales.infoDenunciaUser')
                                         @endforeach
                                     </tbody>
                                 </table>
